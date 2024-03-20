7d612d071346c3d1a366a0f4df9daf258d45bb29
Marcin Bator <marcinbator.ofc@gmail.com>
2024-03-20 08:35:22 +0100

    initial commit

---
+++ src/main/java/pl/immortal/konfero_backend/KonferoBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.immortal.konfero_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonferoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonferoBackendApplication.class, args);
+	}
+
+}
