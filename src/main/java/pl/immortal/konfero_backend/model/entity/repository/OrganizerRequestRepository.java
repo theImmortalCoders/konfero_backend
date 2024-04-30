7166592748781fd074506f93c3c4cedfdc20b4de
Marcin Bator <marcinbator.ofc@gmail.com>
2024-04-30 21:01:22 +0200

    fix: #dev prevent sending multiple organizer requests

--- src/main/java/pl/immortal/konfero_backend/model/entity/repository/OrganizerRequestRepository.java
+++ src/main/java/pl/immortal/konfero_backend/model/entity/repository/OrganizerRequestRepository.java
@@ -2,9 +2,11 @@ package pl.immortal.konfero_backend.model.entity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.immortal.konfero_backend.model.entity.OrganizerRequest;
+import pl.immortal.konfero_backend.model.entity.User;
 
 import java.util.List;
 
 public interface OrganizerRequestRepository extends JpaRepository<OrganizerRequest, Long> {
 	List<OrganizerRequest> findAllByStatus(OrganizerRequest.OrganizerRequestStatus status);
+	Boolean existsByUserAndStatus(User user, OrganizerRequest.OrganizerRequestStatus status);
 }
