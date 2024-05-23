554202779275bb64549dc4c1b883132a52245a05
Marcin Bator <marcinbator.ofc@gmail.com>
2024-05-23 10:06:37 +0200

    feat: #dev deploy test

--- src/main/java/pl/immortal/konfero_backend/KonferoBackendApplication.java
+++ src/main/java/pl/immortal/konfero_backend/KonferoBackendApplication.java
@@ -4,13 +4,18 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
-@OpenAPIDefinition(info = @Info(title = "Konfero backend app documentation", description = "Authors: Marcin Bator, Paweł Buczek"))
+@SpringBootApplication(
+		exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class}
+)
+@OpenAPIDefinition(info = @Info(title = "Konfero backend app documentation", description = "Authors: Marcin Bator"))
 public class KonferoBackendApplication {
 
 	public static void main(String[] args) {
